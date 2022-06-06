2ce7fe85fe73b0a80cfa1cdb52c7e70f12a2b388
pentcle <pentacle@pentacle.xyz>
2022-06-06 21:25:55 +0100

    api talking

--- src/api/author/services/author.js
+++ src/api/author/services/author.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * categorie service.
+ * author service.
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::categorie.categorie');
+module.exports = createCoreService('api::author.author');
