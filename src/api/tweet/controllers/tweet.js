7da92600ec8b5c4089caf0da14269895d1986aa7
Alice <alize_in_cryptoland@protonmail.com>
2022-07-02 05:08:14 +0200

    update ui, fixes

--- src/api/tweet/controllers/tweet.js
+++ src/api/tweet/controllers/tweet.js
@@ -12,26 +12,9 @@ const { createCoreController } = require("@strapi/strapi").factories;
 module.exports = createCoreController("api::tweet.tweet", ({ strapi }) => ({
   async process({ req, res, params }) {
     const tweet_id = params.id;
-
-    const [previousTweet] = await strapi.entityService.findMany(
-      "api::tweet.tweet",
-      { filter: { tweet_id } }
-    );
-
-    if (previousTweet) {
-      const [previousAuthor] = await strapi.entityService.findMany(
-        "api::tweet.twitter-user",
-        { filter: { twitter_user_id: previousTweet.author_id } }
-      );
-      return { tweet: previousTweet, author: previousAuthor };
-    }
-
     const { tweet, author } = await strapi
       .service("api::tweet.tweet")
       .process({ tweet_id });
-
-    await strapi.service("api::tweet.tweet").create({ data: tweet });
-    await strapi.service("api::tweet.twitter-user").create({ data: author });
 
     return { tweet, author };
   },
--- src/api/tweet/services/tweet.js
+++ src/api/tweet/services/tweet.js
@@ -8,8 +8,59 @@ const { createCoreService } = require("@strapi/strapi").factories;
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
+const authorParams = qs.stringify({
+  "user.fields": "name,profile_image_url,description",
+});
+
 module.exports = createCoreService("api::tweet.tweet", ({ strapi }) => ({
   async process({ tweet_id }) {
+    const [previousTweet] = await strapi.entityService.findMany(
+      "api::tweet.tweet",
+      { filters: { tweet_id } }
+    );
+
+    if (previousTweet) {
+      const [previousAuthor] = await strapi.entityService.findMany(
+        "api::tweet.twitter-user",
+        { filters: { twitter_user_id: previousTweet.author_id } }
+      );
+      if (previousAuthor) {
+        return { tweet: previousTweet, author: previousAuthor };
+      } else {
+        const tweetParams = qs.stringify({
+          ids: tweet_id,
+          //   expansions: "attachments.media_keys",
+          "tweet.fields": "created_at,author_id",
+        });
+        const { data } = await fetch(
+          "https://api.twitter.com/2/tweets?" + tweetParams.toString(),
+          { headers: { Authorization: `Bearer ${process.env.TWITTER_BEARER}` } }
+        ).then((res) => res.json());
+        const [{ id: _tweet_id, ...tweetResult }] = data;
+        const author_id = tweetResult.author_id;
+        const res = await fetch(
+          `https://api.twitter.com/2/users/${author_id}?${authorParams.toString()}` +
+            {
+              headers: {
+                Authorization: `Bearer ${process.env.TWITTER_BEARER}`,
+              },
+            }
+        ).then((res) => res.json());
+        const  {
+          data: { id: authorId, ...authorResult },
+        }= res;
+
+        const updatedAuthor = {
+          twitter_user_id: `${author_id}`,
+          ...authorResult,
+        };
+        await strapi
+          .service("api::tweet.twitter-user")
+          .create({ data: updatedAuthor });
+        return { tweet: previousTweet, author: updatedAuthor };
+      }
+    }
+
     const tweetParams = qs.stringify({
       ids: tweet_id,
       //   expansions: "attachments.media_keys",
@@ -21,18 +72,19 @@ module.exports = createCoreService("api::tweet.tweet", ({ strapi }) => ({
     ).then((res) => res.json());
     const [{ id: _tweet_id, ...tweetResult }] = data;
     const author_id = tweetResult.author_id;
-    const authorParams = qs.stringify({
-      "user.fields": "name,verified,profile_image_url,description",
-    });
+
     const [previousAuthor] = await strapi.entityService.findMany(
       "api::tweet.twitter-user",
-      { filter: { twitter_user_id: author_id } }
+      { filters: { twitter_user_id: author_id } }
     );
     const updatedTweet = {
       ...tweetResult,
       tweet_id: `${tweet_id}`,
       twitter_user_id: `${author_id}`,
     };
+
+    await strapi.service("api::tweet.tweet").create({ data: updatedTweet });
+
     if (previousAuthor) {
       return { author: previousAuthor, updatedTweet };
     }
@@ -47,6 +99,10 @@ module.exports = createCoreService("api::tweet.tweet", ({ strapi }) => ({
     ).then((res) => res.json());
 
     const updatedAuthor = { twitter_user_id: `${author_id}`, ...authorResult };
+
+    await strapi
+      .service("api::tweet.twitter-user")
+      .create({ data: updatedAuthor });
 
     return {
       author: updatedAuthor,
