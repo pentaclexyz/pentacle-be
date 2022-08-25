f1bafef0ba2c653bfc41a692a7aa3bd948a5f1da
pentcle <pentacle@pentacle.xyz>
2022-08-25 15:21:14 +0100

    fix: infar settings

--- config/database.js
+++ config/database.js
@@ -5,10 +5,10 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "pentacle_cms_staging"),
-      ssl: env.bool("DATABASE_SSL", false),
+      host: env('PGHOST', '127.0.0.1'),
+      port: env.int('PGPORT', 5432),
+      database: env('PGDATABASE', 'pentacle_cms_staging'),
+      ssl: env.bool(true),
     },
   },
 });
