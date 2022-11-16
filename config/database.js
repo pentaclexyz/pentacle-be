3b924a379b3fbce002e513d30dfebac131be4882
pentacle <pentacle@pentacle.xyz>
2022-11-16 18:04:30 +0000

    Feat/homepage (#106)
    * feat: add feedback to footer
    
    * feat: upgrade strapi

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('PGHOST', '127.0.0.1'),
       port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'pentacle-03'),
+      database: env('PGDATABASE', 'pentacle-04'),
       ssl: env.bool(true),
     },
   },
