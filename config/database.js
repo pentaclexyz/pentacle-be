a15995854e743c6740011bc980f29cf427476fc5
sha237gh <118625539+sha237gh@users.noreply.github.com>
2023-02-20 19:57:23 +0100

    feat: banner override (#775)

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('PGHOST', '127.0.0.1'),
       port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'pentacle-19'),
+      database: env('PGDATABASE', 'pentacle-20'),
       ssl: env.bool(true),
     },
   },
