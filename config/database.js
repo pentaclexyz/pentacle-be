fa5af0b5722857d7bd3775c751e79c48f5e07c1f
pentcle <pentacle@pentacle.xyz>
2023-02-21 16:24:50 +0000

    fix: heroku settings in database

--- config/database.js
+++ config/database.js
@@ -5,9 +5,9 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'pentacle-20'),
+      host: env('HOST', '127.0.0.1'),
+      port: env.int('PORT', 5432),
+      database: env('DATABASE', 'pentacle-20'),
       ssl: env.bool(true),
     },
   },
