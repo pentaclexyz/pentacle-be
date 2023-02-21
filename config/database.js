4e318f4c9d2c550e778728a304d606983b6f0c65
pentcle <pentacle@pentacle.xyz>
2023-02-21 17:01:58 +0000

    fix: heroku settings in database

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "pentacle_cms_staging"),
+      database: env("DATABASE_NAME", "pentacle-26"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
