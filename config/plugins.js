a862a4f05f76abee1ad3393e35826faa9d2ba31f
pentacle <pentacle@pentacle.xyz>
2023-02-25 15:42:54 +0000

    feat: add open ai and some drivey-by icon adds and old file cleaning (#808)
    Signed-off-by: pentacle <pentacle@pentacle.xyz>

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,27 @@
-module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
+module.exports = ({env}) => ({
+  'users-permissions': {
+    config: {
       jwtSecret: env('JWT_SECRET'),
+    },
+  },
+  'open-ai': {
+    enabled: true,
+    config: {
+      API_TOKEN: env('OPENAI_TOKEN'),
+    },
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
-    },
-  });
+  },
+});
