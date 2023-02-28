6ef031b8478abe6f923c3dd454e2283564621517
pentacle <pentacle@pentacle.xyz>
2023-02-28 19:13:16 +0000

    Feat/responsive gov (#834)
    * feat: alchemy
    
    * feat: alchemy
    
    * feat: alchemy
    
    * feat: alchemy
    
    * feat: alchemy
    
    * feat: alchemy
    
    * feat: alchemy
    
    * feat: alchemy

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "pentacle-20"),
+      database: env("DATABASE_NAME", "pentacle-29"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
