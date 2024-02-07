a7262b9faf6ed1a86e372d89dcceed2fbf3fc40c
pentacle <pentacle@pentacle.xyz>
2024-02-07 13:02:43 +0000

    feat: migrate to 4.5.1

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   settings: {
-    forceMigration: false,
+    forceMigration: true,
   },
   connection: {
     client: "postgres",
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "pentacle-heroku-may"),
+      database: env("DATABASE_NAME", "pentacle-feb-2024"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
