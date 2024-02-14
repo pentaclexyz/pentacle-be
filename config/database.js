bb24022d34b85e631f9b67716d352e8c7d89b545
pentacle <pentacle@pentacle.xyz>
2024-02-14 01:05:52 +0000

    feat: documentation

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "pentacle-feb-2024"),
+      database: env("DATABASE_NAME", "p-be"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
--- config/plugins.js
+++ config/plugins.js
@@ -12,50 +12,35 @@ module.exports = ({env}) => ({
   },
   documentation: {
     enabled: true,
-    "openapi": "3.0.0",
-    "info": {
-      "version": "1.0.1",
-      "title": "DOCUMENTATION",
-      "description": "",
-      "termsOfService": "YOUR_TERMS_OF_SERVICE_URL",
-      "contact": {
-        "name": "Pentacle",
-        "email": "pentacle@pentacle.xyz",
-        "url": "https://pentacle.xyz"
+    config: {
+      openapi: '3.0.0',
+      info: {
+        version: '1.0.1',
+        title: 'DOCUMENTATION',
+        description: '',
+        termsOfService: 'YOUR_TERMS_OF_SERVICE_URL',
+        contact: {
+          name: 'TEAM',
+          email: 'contact-email@something.io',
+          url: 'mywebsite.io'
+        },
+        license: {
+          name: 'Apache 2.0',
+          url: 'https://www.apache.org/licenses/LICENSE-2.0.html'
+        },
       },
-      "license": {
-        "name": "Apache 2.0",
-        "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
-      }
-    },
-    "x-strapi-config": {
-      "path": "/documentation",
-      "showGeneratedFiles": true,
-      "generateDefaultResponse": true,
-      "plugins": [
-        "email",
-        "upload",
-        "users-permissions"
-      ]
-    },
-    "servers": [
-      {
-        "url": "https://api.pentacle.xyz",
-        "description": "Pentacle API production server"
-      }
-    ],
-    "externalDocs": {
-      "description": "Find out more",
-      "url": "https://strapi.io/documentation/"
-    },
-    "security": [
-      {
-        "bearerAuth": []
-      }
-    ],
-    "paths": {},
-    "tags": [],
-    "components": {}
+      'x-strapi-config': {
+        // Leave empty to ignore plugins during generation
+        plugins: [ 'upload', 'users-permissions'],
+        path: '/documentation',
+      },
+      servers: [{ url: 'https://api.pentacle.xyz', description: 'Development server' }],
+      externalDocs: {
+        description: 'Find out more',
+        url: 'https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html'
+      },
+      security: [ { bearerAuth: [] } ]
+    }
   },
   upload: {
     config: {
