13c27ca90ee3a700328e048f229a0d183bf3d544
sha237 <sha237@proton.me>
2024-05-19 18:18:33 +0200

    feat: publish projects right away

--- src/api/project/services/project.js
+++ src/api/project/services/project.js
@@ -10,14 +10,12 @@ module.exports = createCoreService("api::project.project", ({ strapi }) => ({
   async createSubmission({ formData, submissionId, ethAddress }) {
     const { eth_address, ...rest } = formData;
     const newItem = await strapi.entityService.create("api::project.project", {
-      data: { ...rest, created_by: ethAddress },
+      data: { ...rest, created_by: ethAddress, publishedAt: new Date() },
     });
     const deleted = await strapi.entityService.delete(
       "api::submission.submission",
       parseInt(submissionId)
     );
-    console.log({ newItem, deleted });
-
-    return newItem;
+    return { newItem, deleted };
   },
 }));
