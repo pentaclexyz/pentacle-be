33beef3ff0e98ddf4b29aac6185aa068d0b96b8f
sha237 <sha237@proton.me>
2024-05-23 19:03:14 +0200

    staging 01

--- src/api/project/services/project.js
+++ src/api/project/services/project.js
@@ -8,9 +8,15 @@ const { createCoreService } = require("@strapi/strapi").factories;
 
 module.exports = createCoreService("api::project.project", ({ strapi }) => ({
   async createSubmission({ formData, submissionId, ethAddress }) {
-    const { eth_address, ...rest } = formData;
+    const { eth_address, profile_img, profile_banner, ...rest } = formData;
     const newItem = await strapi.entityService.create("api::project.project", {
-      data: { ...rest, created_by: ethAddress, publishedAt: new Date() },
+      data: {
+        ...rest,
+        twitter_img: profile_img,
+        twitter_banner: profile_banner,
+        created_by: ethAddress,
+        publishedAt: new Date(),
+      },
     });
     const deleted = await strapi.entityService.delete(
       "api::submission.submission",
