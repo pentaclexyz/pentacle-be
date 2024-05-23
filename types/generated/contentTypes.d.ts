31163213288e85e6380e474b6bc7d586a867fab9
pentacle <pentacle@pentacle.xyz>
2024-05-23 12:13:49 +0100

    feat: add farcster handle not id to person

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1577,7 +1577,7 @@ export interface ApiPersonPerson extends Schema.CollectionType {
       'manyToMany',
       'api::lore.lore'
     >;
-    farcaster_id: Attribute.String;
+    farcaster_handle: Attribute.String;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
