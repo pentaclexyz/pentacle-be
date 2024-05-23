4be4fdc3113ff13f1ab2f2971b53026f0e7a98a6
pentacle <pentacle@pentacle.xyz>
2024-05-23 14:19:31 +0100

    feat: add profile images to be

--- src/api/project/services/project.js
+++ src/api/project/services/project.js
@@ -10,7 +10,13 @@ module.exports = createCoreService("api::project.project", ({ strapi }) => ({
   async createSubmission({ formData, submissionId, ethAddress }) {
     const { eth_address, ...rest } = formData;
     const newItem = await strapi.entityService.create("api::project.project", {
-      data: { ...rest, created_by: ethAddress, publishedAt: new Date() },
+      data: {
+        ...rest,
+        twitter_img: data.profile_img,
+        twitter_banner: data.profile_banner,
+        created_by: ethAddress,
+        publishedAt: new Date(),
+      },
     });
     const deleted = await strapi.entityService.delete(
       "api::submission.submission",
