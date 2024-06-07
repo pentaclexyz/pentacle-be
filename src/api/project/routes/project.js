175597dd6389484b2944573f925746f3a301889a
sha237gh <118625539+sha237gh@users.noreply.github.com>
2024-06-07 12:37:48 +0200

    feat: logger (#74)

--- src/api/project/routes/project.js
+++ src/api/project/routes/project.js
@@ -6,7 +6,13 @@
 
 const { createCoreRouter } = require("@strapi/strapi").factories;
 
-const defaultRouter = createCoreRouter("api::project.project");
+const defaultRouter = createCoreRouter("api::project.project", {
+  config: {
+    find: {
+      middlewares: ['api::project.logger']
+    }
+  }
+});
 const { customRouter } = require("../../../util/custom-router");
 const myExtraRoutes = [
   {
