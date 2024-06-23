f070ede91060c7c02f8b3dc53bc5d4e48a990cb8
sha237 <sha237@proton.me>
2024-06-23 15:29:56 +0200

    fix address

--- src/api/project/services/project.js
+++ src/api/project/services/project.js
@@ -7,14 +7,15 @@
 const { createCoreService } = require("@strapi/strapi").factories;
 
 module.exports = createCoreService("api::project.project", ({ strapi }) => ({
-  async createSubmission({ formData, submissionId, ethAddress }) {
+  async createSubmission({ formData, submissionId }) {
     const { eth_address, profile_img, profile_banner, ...rest } = formData;
     const project = await strapi.entityService.create("api::project.project", {
       data: {
         ...rest,
+        eth_address,
         twitter_img: profile_img,
         twitter_banner: profile_banner,
-        created_by: ethAddress,
+        created_by: eth_address,
         publishedAt: new Date(),
       },
     });
