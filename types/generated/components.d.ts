cd470372cf03c9aff54ab77c59f82d5d17743a13
Yuri <yuripetusko@gmail.com>
2024-07-19 10:30:33 +0200

    build and generate docs

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -34,11 +34,7 @@ export interface SharedNav extends Schema.Component {
   };
   attributes: {
     navTitle: Attribute.String;
-    section: Attribute.Relation<
-      'shared.nav',
-      'oneToOne',
-      'api::section.section'
-    >;
+    section: Attribute.Relation<'shared.nav', 'oneToOne', 'api::section.section'>;
   };
 }
 
@@ -66,16 +62,8 @@ export interface SharedCardLarge extends Schema.Component {
     header: Attribute.String;
     text: Attribute.Text;
     link: Attribute.String;
-    sections: Attribute.Relation<
-      'shared.card-large',
-      'oneToMany',
-      'api::section.section'
-    >;
-    categories: Attribute.Relation<
-      'shared.card-large',
-      'oneToMany',
-      'api::category.category'
-    >;
+    sections: Attribute.Relation<'shared.card-large', 'oneToMany', 'api::section.section'>;
+    categories: Attribute.Relation<'shared.card-large', 'oneToMany', 'api::category.category'>;
     tags: Attribute.Relation<'shared.card-large', 'oneToMany', 'api::tag.tag'>;
     skill_levels: Attribute.Relation<
       'shared.card-large',
@@ -93,11 +81,7 @@ export interface SharedAudits extends Schema.Component {
     description: '';
   };
   attributes: {
-    auditor: Attribute.Relation<
-      'shared.audits',
-      'oneToOne',
-      'api::project.project'
-    >;
+    auditor: Attribute.Relation<'shared.audits', 'oneToOne', 'api::project.project'>;
     audit_url: Attribute.String;
   };
 }
