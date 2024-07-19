2a9d41be3ce20d745a0b090b6e419cdfeb1c9175
Yuri <yuripetusko@gmail.com>
2024-07-19 03:48:38 +0200

    run prettier

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-"use strict";
-import _ from "lodash";
-import { verifyMessage } from "viem";
-import { getHandleFromTwitterUrl } from "./util/util";
-import { Strapi } from "@strapi/strapi";
+'use strict';
+import _ from 'lodash';
+import { verifyMessage } from 'viem';
+import { getHandleFromTwitterUrl } from './util/util';
+import { Strapi } from '@strapi/strapi';
 
 type SocialDataResponse = {
   id: number;
@@ -34,25 +34,25 @@ export default {
    */
   register({ strapi }: { strapi: Strapi }) {
     strapi
-      .plugin("documentation")
-      .service("override")
+      .plugin('documentation')
+      .service('override')
       // TODO: update list
       .excludeFromGeneration([
-        "defi-safety-report",
-        "section",
-        "lore",
-        "tweet",
-        "term",
-        "helper",
-        "homepage",
-        "connect",
-        "auth",
-        "governance-discussion",
-        "governance-proposal",
-        "global",
-        "content-type",
-        "audit",
-        "about",
+        'defi-safety-report',
+        'section',
+        'lore',
+        'tweet',
+        'term',
+        'helper',
+        'homepage',
+        'connect',
+        'auth',
+        'governance-discussion',
+        'governance-proposal',
+        'global',
+        'content-type',
+        'audit',
+        'about',
       ]);
   },
 
@@ -68,58 +68,55 @@ export default {
       async beforeCreate(event) {
         try {
           const ctx = strapi.requestContext.get();
-          if (event.model.singularName === "submission") {
+          if (event.model.singularName === 'submission') {
             const { signature, eth_address, ...rest } = event.params.data;
             event.params.data = { ...rest, eth_address };
             event.params.data.slug = _.kebabCase(event.params.data.slug);
             const isValid = await verifyMessage({
               address: eth_address,
-              message: "Sign this message to submit a project",
+              message: 'Sign this message to submit a project',
               signature,
             });
             if (!isValid) {
-              ctx.throw(400, "Invalid signature");
+              ctx.throw(400, 'Invalid signature');
               return;
             }
           }
 
-          if (event.model.singularName === "person") {
-            event.params.data.twitter =
-              event.params.data?.twitter?.toLowerCase();
+          if (event.model.singularName === 'person') {
+            event.params.data.twitter = event.params.data?.twitter?.toLowerCase();
           }
-          if (event.model.singularName === "project") {
-            event.params.data.twitter_url =
-              event.params.data?.twitter_url?.toLowerCase();
+          if (event.model.singularName === 'project') {
+            event.params.data.twitter_url = event.params.data?.twitter_url?.toLowerCase();
           }
 
-          if (
-            event.model.singularName === "person" ||
-            event.model.singularName === "project"
-          ) {
+          if (event.model.singularName === 'person' || event.model.singularName === 'project') {
             const username =
-              event.model.singularName === "person"
+              event.model.singularName === 'person'
                 ? event.params.data.twitter
                 : event.params.data.twitter_url;
             const response = (await fetch(
-              `https://api.socialdata.tools/twitter/user/${getHandleFromTwitterUrl(
-                username
-              )}`,
+              `https://api.socialdata.tools/twitter/user/${getHandleFromTwitterUrl(username)}`,
               {
                 headers: {
                   Authorization: `Bearer ${process.env.SOCIALDATA_KEY}`,
                 },
-              }
+              },
             ).then((res) => {
               console.log(`API returned :${res.status}: ${res.statusText}`);
               return res.json();
             })) as SocialDataResponse;
             if (!event.params.data.twitter_img) {
-              event.params.data.twitter_img =
-                response.profile_image_url_https?.replace("_normal", "_bigger");
+              event.params.data.twitter_img = response.profile_image_url_https?.replace(
+                '_normal',
+                '_bigger',
+              );
             }
             if (!event.params.data.twitter_banner) {
-              event.params.data.twitter_banner =
-                response.profile_banner_url?.replace("_normal", "_bigger");
+              event.params.data.twitter_banner = response.profile_banner_url?.replace(
+                '_normal',
+                '_bigger',
+              );
             }
           }
 
