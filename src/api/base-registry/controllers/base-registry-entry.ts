e45071d5ba9af0122e84ba5e0daff7c7c7317a0a
Yuri <yuripetusko@gmail.com>
2024-07-20 06:14:34 +0200

    add route to delete broken relations

--- src/api/base-registry/controllers/base-registry-entry.ts
+++ src/api/base-registry/controllers/base-registry-entry.ts
@@ -1,26 +1,44 @@
-"use strict";
+'use strict';
 
 /**
  *  base_registry_entry controller
  */
 
-import { factories } from "@strapi/strapi";
-import { BaseRegistryService } from "../services/base-registry-entry";
+import { factories } from '@strapi/strapi';
+import { BaseRegistryService } from '../services/base-registry-entry';
 
 const { createCoreController } = factories;
 
-export default createCoreController(
-  "api::base-registry.base-registry-entry",
-  ({ strapi }) => ({
-    // Fetch all entries from Base registry API
-    async fetchAllEntriesFromRegistry() {
-      const savedIds = await (
-        strapi.service(
-          "api::base-registry.base-registry-entry"
-        ) as BaseRegistryService
-      ).fetchAndUpdateAllBaseRegistryEntries();
+export default createCoreController('api::base-registry.base-registry-entry', ({ strapi }) => ({
+  // Fetch all entries from Base registry API
+  async fetchAllEntriesFromRegistry() {
+    const savedIds = await (
+      strapi.service('api::base-registry.base-registry-entry') as BaseRegistryService
+    ).fetchAndUpdateAllBaseRegistryEntries();
 
-      return savedIds;
-    },
-  })
-);
+    return savedIds;
+  },
+  async removeAllProjectRelations() {
+    const allEntries = await strapi.entityService?.findMany(
+      'api::base-registry.base-registry-entry',
+      { populate: ['project'], fields: ['id'] },
+    );
+
+    //@ts-ignore
+    for (const entry of allEntries) {
+      if (!!entry?.project?.id) {
+        await strapi.entityService?.update('api::base-registry.base-registry-entry', entry.id, {
+          data: {
+            //@ts-ignore
+            project: {
+              disconnect: [entry.project.id],
+            },
+          },
+          populate: ['project'],
+        });
+      }
+    }
+
+    return true;
+  },
+}));
--- src/api/base-registry/routes/base-registry-entry.ts
+++ src/api/base-registry/routes/base-registry-entry.ts
@@ -1,24 +1,26 @@
-"use strict";
+'use strict';
 
 /**
  * base_registry_entry router.
  */
 
-import { factories } from "@strapi/strapi";
-import { customRouter } from "../../../util/custom-router";
+import { factories } from '@strapi/strapi';
+import { customRouter } from '../../../util/custom-router';
 
 const { createCoreRouter } = factories;
 
-const defaultRouter = createCoreRouter(
-  "api::base-registry.base-registry-entry"
-);
+const defaultRouter = createCoreRouter('api::base-registry.base-registry-entry');
 
 const customRoutes = [
   {
-    method: "GET",
-    path: "/base-registry/fetch-all-entries-from-registry",
-    handler:
-      "api::base-registry.base-registry-entry.fetchAllEntriesFromRegistry",
+    method: 'GET',
+    path: '/base-registry/fetch-all-entries-from-registry',
+    handler: 'api::base-registry.base-registry-entry.fetchAllEntriesFromRegistry',
+  },
+  {
+    method: 'GET',
+    path: '/base-registry/remove-all-project-relations',
+    handler: 'api::base-registry.base-registry-entry.removeAllProjectRelations',
   },
 ];
 
