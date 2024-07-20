c74ff8e1735513875a29f8619a3f8ce4771d4651
Yuri <yuripetusko@gmail.com>
2024-07-20 13:59:21 +0200

    try catch cron tasks

--- config/cron-tasks.ts
+++ config/cron-tasks.ts
@@ -7,11 +7,36 @@ export default {
   },
   // Once a day
   '0 0 * * *': async ({ strapi }: { strapi: Strapi }) => {
-    await strapi.service('api::defi-safety-report.defi-safety-report').fetchReports();
-    await strapi.service('api::tweet.tweet').getAndSetAllProfiles();
-    await strapi.service('api::tweet.tweet').syncTwitterMedia();
-    await strapi.service('api::helper.helper').syncDescriptions();
-    await strapi.service('api::helper.helper').migrateGithub();
+    try {
+      await strapi.service('api::defi-safety-report.defi-safety-report').fetchReports();
+    } catch (e) {
+      console.error('CRON: Error fetchReports', e);
+    }
+
+    try {
+      await strapi.service('api::tweet.tweet').getAndSetAllProfiles();
+    } catch (e) {
+      console.error('CRON: Error getAndSetAllProfiles', e);
+    }
+
+    try {
+      await strapi.service('api::tweet.tweet').syncTwitterMedia();
+    } catch (e) {
+      console.error('CRON: Error syncTwitterMedia', e);
+    }
+
+    try {
+      await strapi.service('api::helper.helper').syncDescriptions();
+    } catch (e) {
+      console.error('CRON: Error syncDescriptions', e);
+    }
+
+    try {
+      await strapi.service('api::helper.helper').migrateGithub();
+    } catch (e) {
+      console.error('CRON: Error migrateGithub', e);
+    }
+
     await strapi
       .service('api::base-registry.base-registry-entry')
       .fetchAndUpdateAllBaseRegistryEntries();
