02c48d33453beca22b8971671c10fbbdee471525
Yuri <yuripetusko@gmail.com>
2024-07-22 13:10:01 +0200

    Add governence discussion status log, also prettier (#94)
    * Add governence discussion status log, also prettier
    
    * Update governance-discussion.js

--- src/api/governance-discussion/services/governance-discussion.js
+++ src/api/governance-discussion/services/governance-discussion.js
@@ -1,32 +1,33 @@
-"use strict";
+'use strict';
 
 /**
  * tag service.
  */
 
-const { createCoreService } = require("@strapi/strapi").factories;
+const { createCoreService } = require('@strapi/strapi').factories;
 
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-const excludeList = ["keep3r-network", "akropolis"];
+const excludeList = ['keep3r-network', 'akropolis'];
 
 module.exports = createCoreService(
-  "api::governance-discussion.governance-discussion",
+  'api::governance-discussion.governance-discussion',
   ({ strapi }) => ({
     async getProjectDiscussions({ discourse_url }) {
-      const discussions = await fetch(`${discourse_url}/latest.json`).then(
-        (res) => res.json()
-      );
+      const discourseUrlWithoutTrailingSlash = discourse_url.replace(/\/$/, '');
+      const discussionsResponse = await fetch(`${discourseUrlWithoutTrailingSlash}/latest.json`);
 
-      return discussions;
+      console.log('discussionsResponse', {
+        url: `${discourseUrlWithoutTrailingSlash}/latest.json`,
+        status: discussionsResponse.status,
+      });
+
+      return discussionsResponse.json();
     },
     async refreshData() {
       console.log(`Starting gov discussions refresh!`);
       const found = (
-        await strapi
-          .service("api::project.project")
-          .find({ pagination: { pageSize: 800 } })
+        await strapi.service('api::project.project').find({ pagination: { pageSize: 800 } })
       ).results.filter((project) => !!project.discourse_url);
 
       for (const project of found) {
@@ -35,39 +36,39 @@ module.exports = createCoreService(
             const discussions = await this.getProjectDiscussions({
               discourse_url: project.discourse_url,
             });
-  
+
             const relevantDiscussions = discussions.topic_list.topics.slice(0, 5);
-  
+
             const previousEntries = await strapi.db
-              .query("api::governance-discussion.governance-discussion")
+              .query('api::governance-discussion.governance-discussion')
               .findMany({
                 where: {
                   project_slug: project.slug,
                 },
               });
-  
+
             if (!previousEntries.length) {
               console.log(`creating discussions for ${project.slug}`);
               await strapi.entityService.create(
-                "api::governance-discussion.governance-discussion",
+                'api::governance-discussion.governance-discussion',
                 {
                   data: {
                     project_slug: project.slug,
                     discussions: relevantDiscussions,
                   },
-                }
+                },
               );
             } else {
               console.log(`updating discussions for ${project.slug}`);
               await strapi.entityService.update(
-                "api::governance-discussion.governance-discussion",
+                'api::governance-discussion.governance-discussion',
                 previousEntries[0].id,
                 {
                   data: {
                     project_slug: project.slug,
                     discussions: relevantDiscussions,
                   },
-                }
+                },
               );
             }
           }
@@ -84,7 +85,7 @@ module.exports = createCoreService(
         return [];
       }
       const discussions = await strapi.db
-        .query("api::governance-discussion.governance-discussion")
+        .query('api::governance-discussion.governance-discussion')
         .findMany({
           where: {
             project_slug: slug,
@@ -95,20 +96,18 @@ module.exports = createCoreService(
     },
     async allWithProject() {
       const discussions = await strapi.db
-        .query("api::governance-discussion.governance-discussion")
+        .query('api::governance-discussion.governance-discussion')
         .findMany();
 
       for (const discussion of discussions) {
-        const projects = await strapi.db
-          .query("api::project.project")
-          .findMany({
-            where: { slug: discussion.project_slug },
-          });
+        const projects = await strapi.db.query('api::project.project').findMany({
+          where: { slug: discussion.project_slug },
+        });
         discussion.project = projects[0];
       }
       return {
         discussions: discussions,
       };
     },
-  })
+  }),
 );
