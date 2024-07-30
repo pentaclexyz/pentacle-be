a16c32dd8e9f5d3b6e77c75f64884b024d06b9ab
Yuri <yuripetusko@gmail.com>
2024-07-30 17:44:32 +0200

    use pentacle ipfs gateway

--- src/util/upload-to-filebase.ts
+++ src/util/upload-to-filebase.ts
@@ -2,13 +2,16 @@ import { FilebaseClient } from '@filebase/client';
 
 const filebaseClient = new FilebaseClient({
   token: process.env.FILEBASE_TOKEN || '',
+  bucket: 'welcome-onchain',
 });
 
 export const uploadToFilebase = async (mediaFile: File | null) => {
   const mediaFileCid = mediaFile ? await filebaseClient.storeBlob(mediaFile) : undefined;
   //TODO: Do we want to save the raw ipfs uri or prepend with ipfs gateway?
-  const mediaUri = mediaFileCid ? `ipfs://${mediaFileCid}` : undefined;
-
+  // const mediaUri = mediaFileCid ? `ipfs://${mediaFileCid}` : undefined;
+  const mediaUri = mediaFileCid
+    ? `https://pentacle.myfilebase.com/ipfs/${mediaFileCid}`
+    : undefined;
   return mediaUri;
 };
 
